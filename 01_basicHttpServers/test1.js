ee5da352e596603f7b15a23d5c1db6b6fe720649
brijesh <brijeshrao333@gmail.com>
2024-06-12 05:08:26 +0530

    Practice

--- 01_basicHttpServers/test1.js
+++ 01_basicHttpServers/test1.js
@@ -1,1 +1,23 @@
-Hi
+const http = require('http');
+
+const localhost ='localhost';
+const PORT = 3000;
+
+
+//Creating request listener
+
+function requestListner(req,res){
+    res.setHeader('content-Type','text/html');
+    res.writeHead(200,'OK');
+    res.end(`<html><body><h1>This is HTML Response</h1></body></html>`);
+}
+
+const server = http.createServer(requestListner)
+
+server.listen(PORT,localhost,(err)=>{
+    if(err){
+        console.log(`failed to start the server`);
+    }else{
+        console.log(`Server started sucess fully`);
+    }
+})
